eee7b56f93542af0917b3fef83e7b934f410e188
bananamonkey4655 <vanillaqicecream@gmail.com>
2021-12-24 21:06:23 +0800

    Added more functionality to calculator, including: decimal places, backspace button & fixed bugs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Calculator operation functions*/
 function add(x, y) {
     return x + y;
 }
@@ -11,33 +12,140 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-    return x / y;
+    return y === 0
+        ? "ERROR! Dividing by zero"
+        : x / y;
 } 
 
 function operate(x, y, operator) {
-    return operator === add
+    x = Math.round(x * 10000000) / 10000000;
+    y = Math.round(y * 10000000) / 10000000;
+
+    console.log(x, y);
+
+    return operator === 'add'
         ? add(x, y)
-        : operator === subtract
+        : operator === 'subtract'
         ? subtract(x, y)
-        : operator === multiply
+        : operator === 'multiply'
         ? multiply(x, y)
-        : operator === divide
+        : operator === 'divide'
         ? divide(x, y)
-        : "ERROR: Invalid operator";
+        : "ERROR! Invalid operator";
+}
+
+/* Main body of script */
+const numberButtons = document.querySelectorAll(".number-btn");
+const operatorButtons = document.querySelectorAll(".operator-btn");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
+const backspaceButton = document.querySelector(".backspace");
+const mainDisplay = document.querySelector(".main-display");
+const subDisplay = document.querySelector(".sub-display");
+
+let leftOperand;
+let rightOperand
+let operator;
+let prevOperator;
+let result;
+let operatorChosen;
+
+clear();
+listen();
+
+function clear() {
+    leftOperand = '';
+    rightOperand = '';
+    result = null;
+    operatorChosen = false;
+    mainDisplay.innerText = '0';
+    subDisplay.innerText = '';
 }
 
 function listen() {
-    const buttons = document.querySelectorAll(".button-grid button");
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", /*Do this when btn clicked*/ (event) => populateDisplay(event));
+    numberButtons.forEach((numBtn) => {
+        numBtn.addEventListener("click", (event) => populateDisplay(event));
+    });
+    operatorButtons.forEach((oprtBtn) => {
+        oprtBtn.addEventListener("click", (event) => callOperator(event));
+    });
+    equalButton.addEventListener("click", () => {
+        if (leftOperand !== '' && mainDisplay.innerText !== '') {
+            getSolution();
+        }
+    });
+    clearButton.addEventListener("click", clear);
+    backspaceButton.addEventListener("click", () => {
+        mainDisplay.innerText = mainDisplay.innerText.slice(0, -1); 
     });
 }
 
 function populateDisplay(event) {
-    const num = event.target.textContent;
-    displayValue = displayValue + num;
-    console.log(displayValue);
+    const input = event.target.innerText;
+    const mainDisplayValue = mainDisplay.innerText;
+
+    //reset main display
+    if (operatorChosen) {
+        mainDisplay.innerText = '';
+        operatorChosen = false;
+    }
+
+    if (mainDisplayValue === '0') {
+        if (input === '.') {
+            mainDisplay.innerText = '0.';
+        } else {
+            mainDisplay.innerText = input;
+        }
+    } else if (input === '.') {
+        if (!mainDisplayValue.includes('.')) {
+            mainDisplay.innerText = mainDisplayValue + '.';
+        }
+    } else {
+        mainDisplay.innerText = mainDisplayValue + input;
+    }
 }
 
-let displayValue = '';
-listen();
+function callOperator(event) {
+    if (result !== null) {
+        leftOperand = result;
+        result = null;
+    } else if (leftOperand === '') {
+        leftOperand = mainDisplay.innerText;
+    } else if (mainDisplay.innerText !== '') {
+        leftOperand = operate(Number(leftOperand), Number(mainDisplay.innerText), operator);
+    }
+
+    operator = event.target.id;   
+
+    operatorChosen = true;
+
+    mainDisplay.innerText = '';
+    displayEquation('half');
+}
+
+function getSolution() {
+    rightOperand = mainDisplay.innerText;
+    result = operate(Number(leftOperand), Number(rightOperand), operator);
+    mainDisplay.innerText = result;
+    displayEquation('full');
+    leftOperand = result;
+    rightOperand = '';
+}
+
+function displayEquation(type) {
+    if (type === 'half') {
+        subDisplay.innerText = `${leftOperand} ${getSymbol(operator)}`;
+    } else {
+        subDisplay.innerText = `${leftOperand} ${getSymbol(operator)} ${rightOperand} =`;
+    }
+}
+
+function getSymbol(opr) {
+    return opr === 'add'
+        ? '+'
+        : opr === 'subtract'
+        ? '−'
+        : opr === 'multiply'
+        ? '×'
+        : '÷';
+}
